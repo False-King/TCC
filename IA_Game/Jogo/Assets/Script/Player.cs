1a3e05feb8ba49043b8dfbd2e40cb0d2dcddb4af
ViniciusHiroki <viniciushiroki1@gmail.com>
2021-04-07 20:14:03 -0300

    Arrumei o pulo e adicionei as animações

--- IA_Game/Jogo/Assets/Script/Player.cs
+++ IA_Game/Jogo/Assets/Script/Player.cs
@@ -9,13 +9,16 @@ public class Player : MonoBehaviour
     private BoxCollider2D boxCollider2d;
     public float speed;
     public float jumpforce;
-    private Rigidbody2D rig;
+    public bool isJumping;
 
+    private Rigidbody2D rig;
+    private Animator anim;
     
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,16 +32,62 @@ public class Player : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime *speed;
+
+        if(Input.GetAxis("Horizontal") > 0f)
+        {
+            anim.SetBool("Run", true);
+            transform.eulerAngles = new Vector3(0f,0f,0f);
+        }
+        if(Input.GetAxis("Horizontal") < 0f)
+        {
+            anim.SetBool("Run", true);
+            transform.eulerAngles = new Vector3(0f,180f,0f);
+        }
+        if(Input.GetAxis("Horizontal") == 0f)
+        {
+            anim.SetBool("Run", false);
+        }
     }
 
     void jump()
     {
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("Jump") && !isJumping)
         {
             rig.AddForce(new Vector2(0f, jumpforce), ForceMode2D.Impulse);
         }
+        if(isJumping==true)
+        {
+            if(Input.GetAxis("Vertical") >= 0f)
+            {
+                anim.SetBool("Jump", true);
+                anim.SetBool("Fall", false);
+            }
+            if(rig.velocity.y < -0.1)
+            {
+                anim.SetBool("Jump", false);
+                anim.SetBool("Fall", true);
+            }
+        }
+        if(isJumping == false)
+        {
+            anim.SetBool("Jump", false);
+            anim.SetBool("Fall", false);
+        }
     }
 
+    void OnCollisionEnter2D (Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            isJumping = false;
+        }
+    }
 
-    
+    void OnCollisionExit2D (Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            isJumping = true;
+        }
+    }
 }
