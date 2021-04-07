3b5da289c80d732fee104135071352b626b23a4c
Marcelo Pereira <tu.marcelo2412@gmail.com>
2021-04-07 13:08:09 -0300

    First commit
    agr vai

---
+++ IA_Game/Jogo/Assets/Script/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Player : MonoBehaviour
+{
+    private BoxCollider2D boxCollider2d;
+    public float speed;
+    public float jumpforce;
+    private Rigidbody2D rig;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+        jump();
+    }
+
+    void move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime *speed;
+    }
+
+    void jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rig.AddForce(new Vector2(0f, jumpforce), ForceMode2D.Impulse);
+        }
+    }
+
+
+    
+}
