14e2725a17f8b9586294edd0b4a731a1fb41fa6d
ViniciusHiroki <viniciushiroki1@gmail.com>
2021-05-11 20:16:50 -0300

    inimigo agora anda
    hehe

--- IA_Game/Jogo/Assets/Script/Enemy.cs
+++ IA_Game/Jogo/Assets/Script/Enemy.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {   
-    public float speed = 2f;
+    public float speed = 3f;
     public Rigidbody2D rb;
     public LayerMask groundLayers;
     public Transform groundCheck;
-    bool isFacingRight;
+    bool isFacingRight = true;
     RaycastHit2D hit;
     // Start is called before the first frame update
     void Start()
@@ -20,6 +20,22 @@ public class Enemy : MonoBehaviour
     private void Update()
     {
         hit = Physics2D.Raycast(groundCheck.position, -transform.up, 1f, groundLayers);
+    }
+
+    private void FixedUpdate()
+    {
+        if(hit.collider != false){
+            if(isFacingRight){
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+            }
+            else{
+                rb.velocity = new Vector2(-speed, rb.velocity.y);
+            }
+        }
+        else{
+            isFacingRight = !isFacingRight;
+            transform.localScale = new Vector3(-transform.localScale.x, 1f, 1f);
+        }
     }
 
     
