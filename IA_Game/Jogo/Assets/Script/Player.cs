26895a06fd16c1da57ab43e44badd03ede7f06c9
ViniciusHiroki <viniciushiroki1@gmail.com>
2021-05-14 17:45:18 -0300

    fiz uns negocio aí

--- IA_Game/Jogo/Assets/Script/Player.cs
+++ IA_Game/Jogo/Assets/Script/Player.cs
@@ -114,5 +114,10 @@ public class Player : MonoBehaviour
         }
     }
 
+    void Win()
+    {
+        
+    }
+
     
 }
--- IA_Game/Jogo/Assets/Script/ProceduralGeneration.cs
+++ IA_Game/Jogo/Assets/Script/ProceduralGeneration.cs
@@ -2,29 +2,33 @@ using System.Data.Common;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
 
 public class ProceduralGeneration : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] int pontos, height;
-    [SerializeField] GameObject bloco, final, inimigo;
+    [SerializeField] int height, x=37;
+    int pontos =  System.Convert.ToInt32(System.IO.File.ReadAllText("../Jogo/Assets/Save/Score.txt"));
+    [SerializeField] GameObject bloco, final, inimigo, blocoP, blocoG;
     void Start()
     {
         generation();
-
     }
     void generation()
     {
-        for (int x = 7; x<pontos-16; x+=8)
+        //for (x = 37; x<pontos; x+=8)
+        while(x<pontos)
         {   
             setNewHeight();
-            Instantiate(bloco, new Vector2(x, height), Quaternion.identity);
+            setPlat();
             if(Random.Range(0, 10)>=7)
             {
-                Instantiate(inimigo, new Vector2(x, height), Quaternion.identity);
+                Instantiate(inimigo, new Vector2(x-32, height), Quaternion.identity);
             }
         }
-        Instantiate(final, new Vector2(pontos, height), Quaternion.identity);
+        setNewHeight();
+        Instantiate(final, new Vector2(x, height), Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -33,8 +37,36 @@ public class ProceduralGeneration : MonoBehaviour
         
     }
 
+    //set uma nova altura da plataforma
     void setNewHeight()
     {
-        height=height+Random.Range(-4, 4);
+        if(height<=2)
+            height=height+Random.Range(0, 4);
+        else if(height>=4)
+            height=height+Random.Range(-4, 0);
+        else
+            height=height+Random.Range(-4, 4);
+    }
+
+    //criação de uma plataforma aleatória
+    void setPlat()
+    {
+        if(Random.Range(0, 10)>=8){
+            Instantiate(blocoP, new Vector2(x, height), Quaternion.identity);
+            x+=4;
+        }
+        else if(Random.Range(0, 10)<=2){
+            Instantiate(blocoG, new Vector2(x, height), Quaternion.identity);
+            x+=14;
+        }
+        else{
+            Instantiate(bloco, new Vector2(x, height), Quaternion.identity);
+            x+=8;
+        }
+    }
+
+    void setTrap()
+    {
+
     }
 }
